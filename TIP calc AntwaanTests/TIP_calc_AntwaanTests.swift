cb88f6e7044d16669e0288e2739ee1a4b9a5748e
user143130 <user143130@telehouse-moboware.dmarc.lga11.atlanticmetro.net>
2018-08-22 07:28:56 -0700

    Initial Commit

---
+++ TIP calc AntwaanTests/TIP_calc_AntwaanTests.swift
@@ -0,0 +1,36 @@
+//
+//  TIP_calc_AntwaanTests.swift
+//  TIP calc AntwaanTests
+//
+//  Created by user143130 on 8/22/18.
+//  Copyright © 2018 Antwaan ThomasMorgan. All rights reserved.
+//
+
+import XCTest
+@testable import TIP_calc_Antwaan
+
+class TIP_calc_AntwaanTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
